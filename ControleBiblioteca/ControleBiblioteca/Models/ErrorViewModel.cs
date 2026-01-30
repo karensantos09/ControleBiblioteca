55d0d5a5bb2202b73488d1ecad4a714ba5024bec
49202470871 <49202470871@ECFP790D1350098>
2026-01-30 08:23:01 -0300

    Adicionar arquivos de projeto.

---
+++ ControleBiblioteca/ControleBiblioteca/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControleBiblioteca.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
